182a0fd859f06fe33e8842659be913e40dfba833
Anton-khv-27 <anto_xa@inbox.ru>
2025-07-29 23:17:39 +0300

    Update telegram_notify.py

--- telegram_notify.py
+++ telegram_notify.py
@@ -3,8 +3,8 @@ import sys
 import os
 
 def send_telegram_message(message):
-    token = os.environ.get("TG_BOT_TOKEN")
-    chat_id = os.environ.get("TG_CHAT_ID")
+    token = os.environ.get("7970951683:AAH8Uw_cCKNIngN-HEqhmnxgYrZrWS0vDqQ")
+    chat_id = os.environ.get("729224032")
     if not token or not chat_id:
         print("Telegram token or chat ID not set")
         return
